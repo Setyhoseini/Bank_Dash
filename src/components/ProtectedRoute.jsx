ab62df3b9909a6845a8ac3f5b0f0d68f1fc2a382
Setyhoseini <setayeshsadathoseini@gmail.com>
2025-04-15 17:07:01 +0430

    Deploy build to gh-pages

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useSelector((state) => state.user);
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
